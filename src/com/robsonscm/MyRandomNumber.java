4984d77ebe6fd12c9a380f3da80139856b45102d
Robson Miranda <robsonscm@gmail.com>
2017-04-17 15:12:05 -0400

    Setters

--- src/com/robsonscm/MyRandomNumber.java
+++ src/com/robsonscm/MyRandomNumber.java
@@ -1,11 +1,12 @@
 package com.robsonscm;
 
-import java.util.Random;
+import java.util.*;
 
 public final class MyRandomNumber extends RandomNumber {
     
     protected int minimum; // this field represents the lowest number the random object should create
     protected int maximum; // this field represents the highest number the random object should create
+    protected int[] possibleMax = {20,100,1000};
     
     
     public MyRandomNumber() {
@@ -23,12 +24,38 @@ public final class MyRandomNumber extends RandomNumber {
     public int getMaximum() {return this.maximum;}
 
     //setters
-    public boolean setMinimum() {
+    public boolean setMinimum(int num) {
+        if (num != 1) {
+            this.minimum = 1;
+            return false;
+        }
+        this.minimum = num;
         return true;
     }
     
-    public boolean setMaximum() {
-        return true;
+    public boolean setMaximum(int num) {
+    
+        Arrays.sort(possibleMax);
+
+        for (int n : possibleMax )
+        {
+            if (n == num)
+            {
+                this.maximum = num;
+                return true;
+            }
+        }
+    
+        System.out.println(possibleMax);
+        
+        for (int max : possibleMax )
+        {
+            if (num <= max)
+            {
+                this.maximum = max;
+                return false;
+            }
+        }
     }
     
 }
